1f68368d69c101ce9f32dd4d3c5812b621b17e9c
Sagir Hossain <sagir.hossain.524@gmail.com>
2022-05-25 01:55:35 +0600

    login page design done

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginComponent } from './pages/login/login.component';
+import { AuthContainerComponent } from './layout/auth-container/auth-container.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent,
+    AuthContainerComponent
+  ],
   imports: [
-    CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
---
+++ src/app/modules/auth/layout/auth-container/auth-container.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-container',
+  templateUrl: './auth-container.component.html',
+  styleUrls: ['./auth-container.component.scss']
+})
+export class AuthContainerComponent {
+  @Input() title: string = '';
+
+  constructor() { }
+}
