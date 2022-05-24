6219a50f6ca65ffa38975480d59506ec99eeb478
Sagir Hossain <sagir.hossain.524@gmail.com>
2022-05-24 23:54:09 +0600

    survey module added

---
+++ src/app/modules/surveys/surveys.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SurveysRoutingModule } from './surveys-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SurveysRoutingModule
+  ]
+})
+export class SurveysModule { }
