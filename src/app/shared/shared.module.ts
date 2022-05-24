cbba0bc30b1e46f4b49c4a6c26888a74c6332e4e
Sagir Hossain <sagir.hossain.524@gmail.com>
2022-05-25 01:00:21 +0600

    basic layout and index page ready

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
   ],
   exports: [
     CommonModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
   ]
 })
 export class SharedModule { }
