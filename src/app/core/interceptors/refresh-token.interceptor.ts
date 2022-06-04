7e5812ef17a42f152e6823da7edd5372ce4a32f0
Sagir Hossain <sagir.hossain.524@gmail.com>
2022-06-04 12:32:40 +0600

    refresh fail login redirect added

--- src/app/core/interceptors/refresh-token.interceptor.ts
+++ src/app/core/interceptors/refresh-token.interceptor.ts
@@ -18,12 +18,14 @@ import { AuthService } from 'src/app/modules/auth/services/auth.service';
 import { LocalStorageService } from '../services/local-storage.service';
 import { User } from '../../modules/auth/models/User';
 import { TokenResponse } from './../../modules/auth/models/TokenResponse';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class RefreshTokenInterceptor implements HttpInterceptor {
   private refreshTokenRequest$: Observable<TokenResponse> | null = null;
 
   constructor(
+    private router: Router,
     private authService: AuthService,
     private storageService: LocalStorageService
   ) {}
@@ -63,6 +65,7 @@ export class RefreshTokenInterceptor implements HttpInterceptor {
             catchError((error) => {
               this.refreshTokenRequest$ = null;
               this.authService.logout();
+              this.router.navigate(['auth', 'login']);
               return throwError(() => error);
             })
           );
